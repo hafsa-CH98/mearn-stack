056bd56ce1650ecbbc32e2e6378de9b993021dcd
hafsa-CH98 <chidmihafsa4@gmail.com>
2024-02-29 15:29:56 +0100

    method put

--- server/server.js
+++ server/server.js
@@ -1,28 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3005;
 const mongoose = require("mongoose");
 require("dotenv").config();
-app.use(express.urlencoded({extended: true}));
-const cors = require('cors');
-const UserModel = require('./models/Users');
-app.use(cors())
-app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }));
+const cors = require("cors");
+const UserModel = require("./models/Users");
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World hello!!");
 });
 
-
 app.post("/", (req, res) => {
   // console.log("Hello  hello the is post valid!!");
-  const newUser = new UserModel(req.body)
-  newUser.save().then(()=> {
-    console.log("seved post")
-
-  }).catch(error => console.log(error))
+  const newUser = new UserModel(req.body);
+  newUser
+    .save()
+    .then(() => {
+      console.log("seved post");
+    })
+    .catch((error) => console.log(error));
+  res.send("seeeeveeeed post")
 });
+
+
+app.put("/:id", async(req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  user.name = req.body.name;
+  user.age = req.body.age;
+  user.email = req.body.email;
+
+
+ user.save()
+ res.json(user)
+} );
+
 
 
 mongoose
